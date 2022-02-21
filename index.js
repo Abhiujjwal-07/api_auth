b0cdf957f3fdd6716bd2a0e2edda2881ef0fb980
Abhiujjwal-07 <Abhiujjwal.pradhan@thewitslab.com>
2022-02-21 19:45:32 +0530

    1

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require("express");
+
 const auth = require("./routes/auth");
 const post = require("./routes/post");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/auth", auth);
 app.use("/post", post);
 app.get("/", (req, res) => {
-    res.send("its working");
+  res.send("its working");
 });
 
 //app.post('/sign' )
 app.listen(3300, () => {
-    console.log("listening on 3300");
+  console.log("listening on 3300");
 });
